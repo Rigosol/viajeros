b60fe628593b11695a2c8f4ccdb7588128df0b71
Miriam2391 <mirita2391@gmail.com>
2024-10-01 07:49:26 +0000

    modificacion

--- web/src/componente/Menu.jsx
+++ web/src/componente/Menu.jsx
@@ -3,6 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import './EstilosMenu.css'
 import { Link } from "react-router-dom";
 
+
 const Menu = () =>{
 
     return (
@@ -10,21 +11,21 @@ const Menu = () =>{
         <>
             <nav className="men">
                 <div className="menu-bar">
-                <Link href="/" className="logo">VIAJEROS<span>.COM</span></Link>
+                <Link to="/" className="logo">VIAJEROS<span>.COM</span></Link>
                 <ul>
-                <li><Link href="/">INICIO</Link></li>
-                <li><Link href="/nosotros">NOSOTROS</Link></li>
-                <li><Link href="/">DESTINOS <i className="bi bi-caret-down"></i> </Link>      
+                <li><Link to="/">INICIO</Link></li>
+                <li><Link to="/nosotros">NOSOTROS</Link></li>
+                <li><Link to="/">DESTINOS <i className="bi bi-caret-down"></i> </Link>      
                 <div className="care-dowm">
                     <ul>
-                        <li><Link href="/">COSTA</Link></li>
-                        <li><Link href="/">SIERRA</Link></li>
-                        <li><Link href="/">SELVA</Link></li>
+                        <li><Link to="/">COSTA</Link></li>
+                        <li><Link to="/">SIERRA</Link></li>
+                        <li><Link to="/">SELVA</Link></li>
                     </ul>
                 </div>
                 </li>
-                <li><Link href="/">TESTIMONIOS</Link></li>
-                <li><Link href="/">CONTACTO</Link></li>
+                <li><Link to="/">TESTIMONIOS</Link></li>
+                <li><Link to="/">CONTACTO</Link></li>
                 </ul>
                 </div>
             </nav>     
