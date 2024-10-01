018230e4c4a12e874fec49b8f40158f592fed0da
Miriam2391 <mirita2391@gmail.com>
2024-10-01 03:25:08 +0000

    instalamos bootstrap react

---
+++ web/src/componente/menu.jsx
@@ -0,0 +1,45 @@
+import { Fragment } from "react";
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './Nosotros.css'
+
+function Menu (){
+
+    return (
+
+        <>
+       <body>
+            <div className="menu-bar">
+            <h1 className="logo">VIAJEROS <span>.COM</span></h1>
+            <ul>
+            <li><a href="#">INICIO</a></li>
+            <li><a href="#">NOSOTROS</a></li>
+            <li><a href="#">DESTINOS <i className="bi bi-caret-down"></i> </a>      
+              <div className="care-dowm">
+                <ul>
+                    <li><a href="#">COSTA</a></li>
+                    <li><a href="#">SIERRA</a></li>
+                    <li><a href="#">SELVA</a></li>
+                </ul>
+              </div>
+            </li>
+            <li><a href="#">TESTIMONIOS</a></li>
+            <li><a href="#">CONTACTO</a></li>
+            </ul>
+            </div>
+
+        <section className="hero">
+            &nbsp;
+        </section>
+
+
+       </body>
+       <section></section>
+
+        
+        </>
+
+    );
+
+}
+
+export default Menu;
