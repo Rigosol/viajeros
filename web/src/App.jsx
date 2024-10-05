05be19dfe20fff92224fa3739fdce17b14023202
Rigosol <ribersoll@gmail.com>
2024-10-05 17:31:38 +0000

    Se actualizaron algunos componentes

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 
 import Menu from './componente/Menu';
-import NosotrosCuerpo from './componente/NosotrosCuerpo';
+import NosotrosCuerpo from './componente/Nosotros';
 import InicioCuerpo from './componente/InicioCuerpo';
 import Contacto from './componente/Contacto';
 
